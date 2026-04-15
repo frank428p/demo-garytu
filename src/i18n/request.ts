4ea1c0798cf686015e4dd6327b9348d7f8801b48
Frank Hsu <frank428p@gmail.com>
2026-04-15 09:56:01 +0800

    i18n file

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -14,6 +14,6 @@ export default getRequestConfig(async () => {
 
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages: (await import(`../../i18n/common/${locale}.json`)).default,
   };
 });
