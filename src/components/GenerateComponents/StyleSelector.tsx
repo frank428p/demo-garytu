b38efc3d24ca71d17cdb0d2c54b692a79cb18562
Frank Hsu <frank428p@gmail.com>
2026-04-17 23:20:58 +0800

    VideoGenerate layout

--- src/components/GenerateComponents/StyleSelector.tsx
+++ src/components/GenerateComponents/StyleSelector.tsx
@@ -6,7 +6,7 @@ import { H4, Small, Tiny } from '../ui/typography';
 
 export function StyleSelector() {
   return (
-    <div className="h-30 w-full aspect-auto lg:aspect-[2.3] cursor-pointer relative group select-none overflow-hidden rounded-xl">
+    <div className="h-25 w-full aspect-auto lg:aspect-[2.3] cursor-pointer relative group select-none overflow-hidden rounded-xl">
       <video
         src={'/video/style_general.mp4'}
         loop
@@ -17,10 +17,10 @@ export function StyleSelector() {
         className="w-full h-full object-cover"
       />
       <div className="absolute w-full bottom-[0px] p-3">
-        <H4 className="text-primary font-extrabold">
+        <H4 className="text-foreground font-extrabold">
           {'Aerial Pullback'.toUpperCase()}
         </H4>
-        <Small>Kling 2.6</Small>
+        {/* <Small>Kling 2.6</Small> */}
       </div>
       <Button className="absolute top-2 right-2 border-none py-1 px-2 h-auto flex items-center gap-1 rounded-full bg-white/10 backdrop-blur-sm hover:bg-primary">
         <IconPencil className="!size-4" />
--- src/components/VideoGenerate/index.tsx
+++ src/components/VideoGenerate/index.tsx
@@ -8,6 +8,7 @@ import {
   IconRun,
   IconMinus,
   IconPlus,
+  IconChevronRight,
 } from '@tabler/icons-react';
 import { useState } from 'react';
 import { Small, TinyMuted } from '../ui/typography';
@@ -24,6 +25,7 @@ import { PromptInput } from '../GenerateComponents/PromptInput';
 import { AudioSelector } from '../GenerateComponents/AudioSelector';
 import { OutputSettingSelector } from '../GenerateComponents/OutputSettingSelector';
 import { FrameUploader } from '../GenerateComponents/FrameUploader';
+import { ScrollArea } from '../ui/scroll-area';
 
 const MAX = 4;
 
@@ -52,77 +54,78 @@ function QuantitySelector() {
 
 const VideoGenerate = () => {
   return (
-    <div className="bg-card/60 p-4 rounded-3xl flex flex-col max-h-[calc(100vh-56px-64px)] lg:max-h-[calc(100vh-56px-12px)]">
-      <div className="flex flex-col gap-2 overflow-y-auto">
-        <div className="flex flex-col gap-2">
-          <StyleSelector />
-          <ModelSelector />
-        </div>
-
-        <div className="flex gap-2">
-          <FrameUploader label="Add a start frame" required />
-          <FrameUploader label="Add an end frame" />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <PromptInput />
-
-          <div className="flex flex-col gap-2 p-2 bg-card rounded-xl">
-            {/* <div className="w-full md:w-[180px] lg:w-[180px] shrink-0"> */}
-            <div className="flex-1 shrink-0">
-              <AudioSelector />
-            </div>
-            <div className="flex-1 shrink-0">
-              <OutputSettingSelector />
-            </div>
+    <div className="bg-card/60 rounded-3xl flex flex-col max-h-[calc(100vh-56px-64px)] lg:max-h-[calc(100vh-56px-12px)]">
+      <ScrollArea className="flex flex-col px-2.5 pt-4 overflow-y-auto flex-1 min-h-0">
+        <div className="flex flex-col gap-2 px-1.5">
+          <div className="flex flex-col gap-2">
+            <StyleSelector />
+            <ModelSelector />
           </div>
 
-          <Collapsible className="rounded-xl data-[state=open]:bg-card">
-            <CollapsibleTrigger asChild>
-              <Button
-                variant="ghost"
-                className="group w-full text-xs rounded-xl hover:bg-transparent data-[state=open]:hover:bg-transparent text-muted-foreground"
-              >
-                Advanced Setting
-                <IconChevronDown className="ml-auto group-data-[state=open]:rotate-180" />
-              </Button>
-            </CollapsibleTrigger>
-            <CollapsibleContent className="flex flex-col items-start gap-2 p-2 pt-0 text-sm">
-              {/* <div className="flex-1 flex justify-between h-auto rounded-xl p-2 bg-secondary items-center hover:bg-secondary/80 transition-colors cursor-pointer"> */}
-              <div className="flex w-full justify-between h-auto rounded-xl p-2 bg-secondary items-center hover:bg-secondary/80 transition-colors cursor-pointer">
-                <div className="flex gap-2 flex-1">
-                  <div className="p-3 rounded-xl bg-ring/20">
-                    <IconCamera className="!h-6 !w-6" />
-                  </div>
-                  <div className="flex flex-col items-start flex-1">
-                    <TinyMuted className="py-1">Movement</TinyMuted>
-                    <div className="flex items-center gap-1 h-4">
-                      <Small className="leading-5">Auto</Small>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <div className="flex gap-2">
+            <FrameUploader label="Add a start frame" required />
+            <FrameUploader label="Add an end frame" />
+          </div>
 
-              {/* <div className="flex-1 flex justify-between h-auto rounded-xl p-2 bg-secondary items-center hover:bg-secondary/80 transition-colors cursor-pointer"> */}
-              <div className="flex w-full justify-between h-auto rounded-xl p-2 bg-secondary items-center hover:bg-secondary/80 transition-colors cursor-pointer">
-                <div className="flex gap-2 flex-1">
-                  <div className="p-3 rounded-xl bg-ring/20">
-                    <IconRun className="!h-6 !w-6" />
-                  </div>
-                  <div className="flex flex-col items-start flex-1">
-                    <TinyMuted className="py-1">Motion</TinyMuted>
-                    <div className="flex items-center gap-1 h-4">
-                      <Small className="leading-5">Off</Small>
+          <div className="flex flex-col gap-2">
+            <PromptInput />
+
+            <div className="flex flex-col gap-2 p-2 bg-card rounded-xl">
+              <div className="flex-1 shrink-0">
+                <AudioSelector />
+              </div>
+              <div className="flex-1 shrink-0">
+                <OutputSettingSelector />
+              </div>
+            </div>
+
+            <Collapsible className="rounded-xl data-[state=open]:bg-card">
+              <CollapsibleTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="group w-full text-xs rounded-xl hover:bg-transparent data-[state=open]:hover:bg-transparent text-muted-foreground"
+                >
+                  Advanced Setting
+                  <IconChevronDown className="ml-auto group-data-[state=open]:rotate-180" />
+                </Button>
+              </CollapsibleTrigger>
+              <CollapsibleContent className="flex flex-col items-start gap-2 p-2 pt-0 text-sm">
+                <div className="flex w-full justify-between h-auto rounded-xl p-2 bg-secondary items-center hover:bg-secondary/80 transition-colors cursor-pointer">
+                  <div className="flex gap-2 flex-1">
+                    <div className="p-3 rounded-xl bg-ring/20">
+                      <IconCamera className="!h-6 !w-6" />
+                    </div>
+                    <div className="flex flex-col items-start flex-1">
+                      <TinyMuted className="py-1">Movement</TinyMuted>
+                      <div className="flex items-center gap-1 h-4">
+                        <Small className="leading-5">Auto</Small>
+                      </div>
                     </div>
                   </div>
+                  <IconChevronRight size={16} />
                 </div>
-              </div>
-            </CollapsibleContent>
-          </Collapsible>
+
+                <div className="flex w-full justify-between h-auto rounded-xl p-2 bg-secondary items-center hover:bg-secondary/80 transition-colors cursor-pointer">
+                  <div className="flex gap-2 flex-1">
+                    <div className="p-3 rounded-xl bg-ring/20">
+                      <IconRun className="!h-6 !w-6" />
+                    </div>
+                    <div className="flex flex-col items-start flex-1">
+                      <TinyMuted className="py-1">Motion</TinyMuted>
+                      <div className="flex items-center gap-1 h-4">
+                        <Small className="leading-5">Off</Small>
+                      </div>
+                    </div>
+                  </div>
+                  <IconChevronRight size={16} />
+                </div>
+              </CollapsibleContent>
+            </Collapsible>
+          </div>
         </div>
-      </div>
+      </ScrollArea>
 
-      <div className="mt-2 flex items-center gap-2 mb-1">
+      <div className="mt-2 flex items-center gap-2 mb-1 shrink-0 px-4 pb-4">
         <QuantitySelector />
         <Button className="flex-1 flex items-center text-base h-11 shadow-[0_4px_0_0_color-mix(in_oklch,var(--color-primary)_70%,black)] active:translate-y-[3px] transition-all duration-75">
           Generate
