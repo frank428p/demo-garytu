3352dd1398c6c115941c482235dafa24f487b708
Frank Hsu <frank428p@gmail.com>
2026-05-04 12:09:57 +0800

    checkout page

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+import { Card } from '@/components/ui/card';
+import { H3 } from '@/components/ui/typography';
+
 const CheckoutSuccessPage = () => {
-  return <>CheckoutSuccessPage</>;
+  return (
+    <>
+      <div>
+        <Card className="max-w-xl p-6 bg-card">
+          <div className="flex flex-col items-center">
+            <H3>Payment Successful</H3>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default CheckoutSuccessPage;
